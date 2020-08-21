dbe9f0c4bd356e0998acad5b269a579cff8900eb
ikethecoder <ikethecoder@canzea.com>
2020-08-21 15:15:14 -0700

    Update main.py

--- argg_api/main.py
+++ argg_api/main.py
@@ -244,9 +244,9 @@ def create_package(req_data):
   package_dict = {
     "title": req_data["metadata_details"].get("title"),
     "name": prepare_package_name(req_data["metadata_details"].get("title")),
-    "org": settings.BCDC_PACKAGE_OWNER_ORG_ID,
-    "sub_org": settings.BCDC_PACKAGE_OWNER_SUB_ORG_ID,
-    "owner_org": settings.BCDC_PACKAGE_OWNER_SUB_ORG_ID,
+    "org": req_data["metadata_details"]["owner"]["contact_person"].get("org_id", settings.BCDC_PACKAGE_OWNER_ORG_ID),
+    "sub_org": req_data["metadata_details"]["owner"]["contact_person"].get("sub_org_id", settings.BCDC_PACKAGE_OWNER_SUB_ORG_ID),
+    "owner_org": req_data["metadata_details"]["owner"]["contact_person"].get("sub_org_id", settings.BCDC_PACKAGE_OWNER_SUB_ORG_ID),
     "notes": req_data["metadata_details"].get("description"),
     "groups": [{"id" : settings.BCDC_GROUP_ID}],
     "state": "active",
